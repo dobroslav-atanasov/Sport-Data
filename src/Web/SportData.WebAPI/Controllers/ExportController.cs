67024a881a3fd4b71a9ace227f0239dce63ae43b
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-03-17 12:13:10 +0200

    api project update

---
+++ src/Web/SportData.WebAPI/Controllers/ExportController.cs
@@ -0,0 +1,21 @@
+﻿namespace SportData.WebAPI.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ExportController : ControllerBase
+{
+    private readonly ILogger<ExportController> _logger;
+
+    public ExportController(ILogger<ExportController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("Add")]
+    public IActionResult Add()
+    {
+        return this.Ok("Add");
+    }
+}
