1ebdf224c4217099baf3b417daa71b86afb397d7
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-03-23 10:00:02 +0200

    Create file with global constants.

---
+++ src/Common/SportData.Common.Constants/AppGlobalConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace SportData.Common.Constants;
+
+public static class AppGlobalConstants
+{
+    public const string LOGGING = "Logging";
+    public const string LOG4NET_CORE = "Log4NetCore";
+}
