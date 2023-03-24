9fe0fe178bb5dac72ea4751a05072663c94ad30e
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-03-24 15:22:07 +0200

    Update constants.

--- src/Common/SportData.Common.Constants/AppGlobalConstants.cs
+++ src/Common/SportData.Common.Constants/AppGlobalConstants.cs
@@ -4,4 +4,13 @@ public static class AppGlobalConstants
 {
     public const string LOGGING = "Logging";
     public const string LOG4NET_CORE = "Log4NetCore";
+
+    public const string USERS_CONNECTION_STRING = "UsersConnectionString";
+
+
+    public const string JWT_SECRET = "JWT:Secret";
+    public const string JWT_ISSUER = "JWT:Issuer";
+    public const string JWT_AUDIENCE = "JWT:Audience";
+    public const string JWT_SUBJECT = "JWT:Subject";
+
 }
