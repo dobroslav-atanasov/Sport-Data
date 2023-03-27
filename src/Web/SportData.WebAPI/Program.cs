56dc0f35b3174f82d51fd70e5b4543270d948bcf
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-03-27 09:50:30 +0300

    Program.cs is updated.

--- src/Web/SportData.WebAPI/Program.cs
+++ src/Web/SportData.WebAPI/Program.cs
@@ -1,9 +1,17 @@
 namespace SportData.WebAPI;
 
+using System.Text;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 using SportData.Common.Constants;
 using SportData.Data.Contexts;
+using SportData.Data.Models.Entities.Users;
+using SportData.WebAPI.Infrastructure.Filters.Swagger;
 
 public class Program
 {
@@ -21,7 +29,7 @@ public class Program
         var services = builder.Services;
         var configuration = builder.Configuration;
 
-        // LOGGING
+        // Logging
         services.AddLogging(config =>
         {
             config.AddConfiguration(configuration.GetSection(AppGlobalConstants.LOGGING));
@@ -29,12 +37,56 @@ public class Program
             config.AddLog4Net(configuration.GetSection(AppGlobalConstants.LOG4NET_CORE).Get<Log4NetProviderOptions>());
         });
 
+        // Users Db
+        services.AddDbContext<UserDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString(AppGlobalConstants.USERS_CONNECTION_STRING)).UseLazyLoadingProxies();
+        });
+
+        // Config identity
+        services.AddIdentity<User, IdentityRole>()
+            .AddEntityFrameworkStores<UserDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.Configure<IdentityOptions>(options =>
+        {
+            options.SignIn.RequireConfirmedEmail = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequiredLength = 4;
+            options.Password.RequiredUniqueChars = 0;
+        });
+
+        // JWT Authentication
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.SaveToken = true;
+            options.RequireHttpsMetadata = false;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidAudience = configuration.GetValue<string>(AppGlobalConstants.JWT_AUDIENCE),
+                ValidIssuer = configuration.GetValue<string>(AppGlobalConstants.JWT_ISSUER),
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetValue<string>(AppGlobalConstants.JWT_SECRET)))
+            };
+        });
+
         services.AddControllers();
         services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen();
-
-        services.AddDbContext<UserDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("BloggingContext")));
-        services.AddDbContext<OlympicGamesDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("BloggingContext")));
+        services.AddSwaggerGen(options =>
+        {
+            options.EnableAnnotations();
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "SportData123", Version = "v1" });
+            options.SchemaFilter<SwaggerSchemaExampleFilter>();
+            options.OperationFilter<SwaggerTestOperationFilter>();
+        });
     }
 
     private static void Configure(WebApplication app)
@@ -47,9 +99,8 @@ public class Program
         }
 
         app.UseHttpsRedirection();
-
+        app.UseAuthentication();
         app.UseAuthorization();
-
         app.MapControllers();
     }
 }
