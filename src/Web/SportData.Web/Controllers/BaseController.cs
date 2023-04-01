5194eca49a56eeaaa299f329fb05c2bf2fdf2bd9
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-04-01 15:45:27 +0300

    Base controller is created.

---
+++ src/Web/SportData.Web/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿namespace SportData.Web.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+public abstract class BaseController : Controller
+{
+    protected BaseController(ILogger<BaseController> logger)
+    {
+        this.Logger = logger;
+    }
+
+    protected ILogger<BaseController> Logger { get; }
+}
