fef9423c4d737ebdbcfddd13af873951d5c9b803
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-04-03 13:07:08 +0300

    Create base entity model.

---
+++ src/Data/SportData.Data.Models/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace SportData.Data.Models.Entities;
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+using SportData.Data.Models.Entities.Interfaces;
+
+public abstract class BaseEntity<TKey> : IAuditEntity
+{
+    [Key]
+    public TKey Id { get; set; }
+
+    public DateTime CreatedOn { get; set; }
+
+    public DateTime? ModifiedOn { get; set; }
+}
