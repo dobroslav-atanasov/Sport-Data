1953bd81e4bd505c419d8c39490f1cf067d3af5a
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-04-03 13:07:37 +0300

    Create db context.

---
+++ src/Data/SportData.Data/Contexts/SportDataDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace SportData.Data.Contexts;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+using SportData.Data.Models.Entities;
+
+public class SportDataDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+{
+    public SportDataDbContext(DbContextOptions<SportDataDbContext> options)
+        : base(options)
+    {
+    }
+}
