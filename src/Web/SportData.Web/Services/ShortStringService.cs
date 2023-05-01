d567526f771bc68b136a30d93afddb488314ed8f
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-05-01 13:51:19 +0300

    Create a service.

---
+++ src/Web/SportData.Web/Services/ShortStringService.cs
@@ -0,0 +1,24 @@
+﻿namespace SportData.Web.Services;
+
+public interface IShortStringService
+{
+    string GetShort(string text, int minLength);
+}
+
+public class ShortStringService : IShortStringService
+{
+    public string GetShort(string text, int minLength)
+    {
+        if (string.IsNullOrEmpty(text))
+        {
+            return text;
+        }
+
+        if (text.Length <= minLength)
+        {
+            return text;
+        }
+
+        return text.Substring(0, minLength) + "...";
+    }
+}
