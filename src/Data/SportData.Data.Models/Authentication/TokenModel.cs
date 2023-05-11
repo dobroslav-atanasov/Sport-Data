0e04888384f52fc919a25cdd7067a6dbf7c254fb
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-05-11 11:00:28 +0300

    TokenModel is created.

---
+++ src/Data/SportData.Data.Models/Authentication/TokenModel.cs
@@ -0,0 +1,16 @@
+﻿namespace SportData.Data.Models.Authentication;
+
+using Swashbuckle.AspNetCore.Annotations;
+
+[SwaggerSchema(Description = "Token model")]
+public class TokenModel
+{
+    [SwaggerSchema(Description = "This is test")]
+    public string AccessToken { get; set; }
+
+    /// <summary>
+    /// Refresh token
+    /// </summary>
+    /// <example>asd</example>
+    public string RefreshToken { get; set; }
+}
