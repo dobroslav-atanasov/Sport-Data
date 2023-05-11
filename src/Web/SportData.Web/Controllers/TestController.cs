4da33a708ef8c0ddd270c118151096e83938f02a
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-05-11 11:32:21 +0300

    Added controller.

---
+++ src/Web/SportData.Web/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿namespace SportData.Web.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+public class TestController : BaseController
+{
+    public TestController(ILogger<BaseController> logger)
+        : base(logger)
+    {
+    }
+
+    public IActionResult Add(int id, string name)
+    {
+        return this.Json(new { id, name });
+    }
+}
