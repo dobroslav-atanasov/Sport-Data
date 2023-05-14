b8701d84370484625f72b5b7e009ef6fb9caaf2f
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-05-14 14:24:47 +0300

    Create application role.

---
+++ src/Data/SportData.Data.Models/Entities/ApplicationRole.cs
@@ -0,0 +1,18 @@
+﻿namespace SportData.Data.Models.Entities;
+
+using System;
+
+using Microsoft.AspNetCore.Identity;
+
+using SportData.Data.Models.Entities.Interfaces;
+
+public class ApplicationRole : IdentityRole, IAuditEntity, IDeletableEntity
+{
+    public DateTime CreatedOn { get; set; }
+
+    public DateTime? ModifiedOn { get; set; }
+
+    public bool IsDeletable { get; set; }
+
+    public DateTime? DeletedOn { get; set; }
+}
