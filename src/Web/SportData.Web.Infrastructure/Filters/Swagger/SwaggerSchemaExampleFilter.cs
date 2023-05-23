4499b1346e9e005d3d13c4e0f0064cb7b857abdb
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-05-23 14:01:52 +0300

    Filters created.

---
+++ src/Web/SportData.Web.Infrastructure/Filters/Swagger/SwaggerSchemaExampleFilter.cs
@@ -0,0 +1,25 @@
+﻿namespace SportData.Web.Infrastructure.Filters.Swagger;
+
+using System.Reflection;
+
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+
+using SportData.Web.Infrastructure.Attributes;
+
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+public class SwaggerSchemaExampleFilter : ISchemaFilter
+{
+    public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+    {
+        if (context.MemberInfo != null)
+        {
+            var schemaAttribute = context.MemberInfo.GetCustomAttributes<SwaggerSchemaExampleAttribute>().FirstOrDefault();
+            if (schemaAttribute != null && schemaAttribute.Example != null)
+            {
+                schema.Example = new OpenApiString(schemaAttribute.Example);
+            }
+        }
+    }
+}
