a5b11c5ef729289e25e9a660817ae06e9e667e2f
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-05-29 09:48:26 +0300

    Property remove.

--- src/Web/SportData.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/Web/SportData.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -60,9 +60,6 @@ public class LoginModel : PageModel
         [EmailAddress]
         public string Email { get; set; }
 
-        [Required]
-        public string Username { get; set; }
-
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
