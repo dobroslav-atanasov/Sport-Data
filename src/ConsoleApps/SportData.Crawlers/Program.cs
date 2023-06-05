bc9363327e5dd0c1e9d549a88bcea98e3dc03fd7
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-06-05 09:16:18 +0300

    Program.cs update

--- src/ConsoleApps/SportData.Crawlers/Program.cs
+++ src/ConsoleApps/SportData.Crawlers/Program.cs
@@ -1,9 +1,48 @@
 ﻿namespace SportData.Crawlers;
 
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using SportData.Common.Constants;
+using SportData.Services;
+using SportData.Services.Interfaces;
+
 public class Program
 {
     public static async Task Main(string[] args)
     {
-        ;
+        var services = ConfigureServices();
+    }
+
+    private static ServiceProvider ConfigureServices()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile(AppGlobalConstants.APP_SETTINGS_FILE, false, true)
+            .Build();
+
+        var services = new ServiceCollection();
+
+        // CONFIGURATION
+        services.AddSingleton<IConfiguration>(configuration);
+
+        // LOGGING
+        services.AddLogging(options =>
+        {
+            options.AddConfiguration(configuration.GetSection(AppGlobalConstants.LOGGING));
+            options.AddConsole();
+            options.AddLog4Net(configuration.GetSection(AppGlobalConstants.LOG4NET_CORE).Get<Log4NetProviderOptions>());
+        });
+
+        // DATABASE
+        //var crawlerStorageOptions = new DbContextOptionsBuilder<CrawlerStorageDbContext>()
+        //    .UseLazyLoadingProxies(true)
+        //    .UseSqlServer(configuration.GetConnectionString(AppGlobalConstants.CRAWLER_STORAGE_CONNECTION_STRING))
+        //    .Options;
+
+        services.AddScoped<IHttpService, HttpService>();
+
+        var serviceProvider = services.BuildServiceProvider();
+        return serviceProvider;
     }
 }
