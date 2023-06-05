ff8a599c508b6b846cb2d64d61b233f078417171
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-06-05 09:18:15 +0300

    Abstract class BaseCrawler create

---
+++ src/ConsoleApps/SportData.Crawlers/BaseCrawler.cs
@@ -0,0 +1,24 @@
+﻿namespace SportData.Crawlers;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+using SportData.Services.Interfaces;
+
+public abstract class BaseCrawler
+{
+    public BaseCrawler(ILogger<BaseCrawler> logger, IConfiguration configuration, IHttpService httpService)
+    {
+        Logger = logger;
+        Configuration = configuration;
+        HttpService = httpService;
+    }
+
+    protected ILogger<BaseCrawler> Logger { get; }
+
+    protected IConfiguration Configuration { get; }
+
+    protected IHttpService HttpService { get; }
+
+    public abstract Task StartAsync();
+}
