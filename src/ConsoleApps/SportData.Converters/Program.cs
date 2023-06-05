9f1b028d02df6dc47235411bac8f1439472382a7
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-06-05 09:15:24 +0300

    The part of class is implemented.

--- src/ConsoleApps/SportData.Converters/Program.cs
+++ src/ConsoleApps/SportData.Converters/Program.cs
@@ -1,9 +1,38 @@
 ﻿namespace SportData.Converters;
 
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using SportData.Common.Constants;
+
 public class Program
 {
     public static async Task Main(string[] args)
     {
+        var services = ConfigureServices();
+    }
 
+    private static ServiceProvider ConfigureServices()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile(AppGlobalConstants.APP_SETTINGS_FILE, false, true)
+            .Build();
+
+        var services = new ServiceCollection();
+
+        // CONFIGURATION
+        services.AddSingleton<IConfiguration>(configuration);
+
+        // LOGGING
+        services.AddLogging(config =>
+        {
+            config.AddConfiguration(configuration.GetSection(AppGlobalConstants.LOGGING));
+            config.AddConsole();
+            config.AddLog4Net(configuration.GetSection(AppGlobalConstants.LOG4NET_CORE).Get<Log4NetProviderOptions>());
+        });
+
+        var serviceProvider = services.BuildServiceProvider();
+        return serviceProvider;
     }
 }
