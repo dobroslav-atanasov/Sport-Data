d7c797eb2a2054edd7f7013dd71dada2c0edc713
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-06-07 10:43:38 +0300

    BaseCrawler modify

--- src/ConsoleApps/SportData.Crawlers/BaseCrawler.cs
+++ src/ConsoleApps/SportData.Crawlers/BaseCrawler.cs
@@ -3,6 +3,8 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
+using SportData.Data.Entities.Crawlers;
+using SportData.Data.Models.Http;
 using SportData.Services.Interfaces;
 
 public abstract class BaseCrawler
@@ -21,4 +23,67 @@ public abstract class BaseCrawler
     protected IHttpService HttpService { get; }
 
     public abstract Task StartAsync();
+
+    protected async Task ProcessGroupAsync(HttpModel httpModel)
+    {
+        var name = this.GetNameFromUrl(httpModel);
+        var document = this.CreateDocument(httpModel);
+
+        var group = new Group
+        {
+            Name = name,
+            CrawlerId = this.CrawlerId.Value,
+            Documents = new List<Document> { document }
+        };
+
+        await this.GroupsService.AddOrUpdateGroupAsync(group);
+    }
+
+    protected async Task ProcessGroupAsync(HttpModel httpModel, ICollection<Document> documents)
+    {
+        var name = this.GetNameFromUrl(httpModel);
+
+        var group = new Group
+        {
+            Name = name,
+            CrawlerId = this.CrawlerId.Value,
+            Documents = documents
+        };
+
+        await this.GroupsService.AddOrUpdateGroupAsync(group);
+    }
+
+    protected Document CreateDocument(HttpModel httpModel)
+    {
+        var name = this.GetNameFromUrl(httpModel);
+        var document = new Document
+        {
+            Name = $"{name}.html".ToLower(),
+            Format = httpModel.MimeType,
+            Url = httpModel.Url,
+            Content = httpModel.Bytes,
+            Encoding = httpModel.Encoding.BodyName,
+            Order = 1
+        };
+
+        return document;
+    }
+
+    protected virtual string GetNameFromUrl(HttpModel httpModel)
+    {
+        var uri = httpModel.Uri;
+        var nameParts = uri.AbsolutePath.Split(new[] { "/" }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+        var name = string.Join("_", nameParts);
+        name = name.Replace(".html", string.Empty);
+
+        return name;
+    }
+
+    protected string CreateUrl(string link, string mainUrl)
+    {
+        var mainUri = new Uri(mainUrl);
+        var uri = new Uri(mainUri, link);
+
+        return uri.AbsoluteUri;
+    }
 }
