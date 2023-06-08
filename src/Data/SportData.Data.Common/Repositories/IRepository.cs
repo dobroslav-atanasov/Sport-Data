14b9fd0d330cee15ea763a2cc6563326ff8f3d7b
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-06-08 09:53:31 +0300

    Repository pattern introduce.

---
+++ src/Data/SportData.Data.Common/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace SportData.Data.Common.Repositories;
+
+public interface IRepository<TEntity>
+    where TEntity : class
+{
+    Task<IQueryable<TEntity>> FindAllAsync();
+
+    Task AddAsync(TEntity entity);
+
+    Task UpdateAsync(TEntity entity);
+
+    Task DeleteAsync(TEntity entity);
+
+    Task SaveChangesAsync();
+}
