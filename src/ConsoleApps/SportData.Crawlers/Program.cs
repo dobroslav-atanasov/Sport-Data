35944d593998938b9f625f7f7b58997e5ae08c0b
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-06-14 08:39:55 +0300

    Services registered to dependency container.

--- src/ConsoleApps/SportData.Crawlers/Program.cs
+++ src/ConsoleApps/SportData.Crawlers/Program.cs
@@ -1,11 +1,16 @@
 ﻿namespace SportData.Crawlers;
 
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 using SportData.Common.Constants;
+using SportData.Crawlers.Countries;
+using SportData.Data.Contexts;
 using SportData.Services;
+using SportData.Services.Data.CrawlerStorage;
+using SportData.Services.Data.CrawlerStorage.Interfaces;
 using SportData.Services.Interfaces;
 
 public class Program
@@ -13,6 +18,13 @@ public class Program
     public static async Task Main(string[] args)
     {
         var services = ConfigureServices();
+        await StartCrawlersAsync(services);
+    }
+
+    private static async Task StartCrawlersAsync(ServiceProvider services)
+    {
+        await services.GetService<CountryDataCrawler>().StartAsync();
+
     }
 
     private static ServiceProvider ConfigureServices()
@@ -35,12 +47,26 @@ public class Program
         });
 
         // DATABASE
+        services.AddDbContext<CrawlerStorageDbContext>(options =>
+        {
+            options.UseLazyLoadingProxies();
+            options.UseSqlServer(configuration.GetConnectionString(AppGlobalConstants.CRAWLER_STORAGE_CONNECTION_STRING));
+        });
         //var crawlerStorageOptions = new DbContextOptionsBuilder<CrawlerStorageDbContext>()
         //    .UseLazyLoadingProxies(true)
         //    .UseSqlServer(configuration.GetConnectionString(AppGlobalConstants.CRAWLER_STORAGE_CONNECTION_STRING))
         //    .Options;
 
         services.AddScoped<IHttpService, HttpService>();
+        services.AddScoped<IMD5Hash, MD5Hash>();
+        services.AddScoped<IZipService, ZipService>();
+
+        services.AddScoped<ICrawlersService, CrawlersService>();
+        services.AddScoped<IGroupsService, GroupsService>();
+        services.AddScoped<ILogsService, LogsService>();
+        services.AddScoped<IOperationsService, OperationsService>();
+
+        services.AddTransient<CountryDataCrawler>();
 
         var serviceProvider = services.BuildServiceProvider();
         return serviceProvider;
