e0f6c706b4856c6b97bbce70cc74c73225f0005b
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-06-15 16:06:13 +0300

    Base converter is created.

---
+++ src/ConsoleApps/SportData.Converters/BaseConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace SportData.Converters;
+
+using Microsoft.Extensions.Logging;
+
+public abstract class BaseConverter
+{
+    public BaseConverter(ILogger<BaseConverter> logger)
+    {
+        this.Logger = logger;
+    }
+
+    protected ILogger<BaseConverter> Logger { get; }
+
+    protected abstract Task ProcessGroupAsync(Group group);
+}
