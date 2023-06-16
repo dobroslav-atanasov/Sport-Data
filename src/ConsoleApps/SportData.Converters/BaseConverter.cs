cd58c3d3e628b0e6706d49462b557feb0381c9fb
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-06-16 09:26:39 +0300

    The base converter was modified.

--- src/ConsoleApps/SportData.Converters/BaseConverter.cs
+++ src/ConsoleApps/SportData.Converters/BaseConverter.cs
@@ -1,15 +1,91 @@
 ﻿namespace SportData.Converters;
 
+using System.Text;
+
+using Dasync.Collections;
+
+using HtmlAgilityPack;
+
 using Microsoft.Extensions.Logging;
+
+using SportData.Common.Extensions;
+using SportData.Data.Entities.Crawlers;
+using SportData.Services.Data.CrawlerStorage.Interfaces;
+using SportData.Services.Interfaces;
 
 public abstract class BaseConverter
 {
-    public BaseConverter(ILogger<BaseConverter> logger)
+    private readonly ICrawlersService crawlersService;
+    private readonly ILogsService logsService;
+    private readonly IGroupsService groupsService;
+    private readonly IZipService zipService;
+
+    public BaseConverter(ILogger<BaseConverter> logger, ICrawlersService crawlersService, ILogsService logsService, IGroupsService groupsService, IZipService zipService,
+        IRegularExpressionService regularExpressionService)
     {
         this.Logger = logger;
+        this.crawlersService = crawlersService;
+        this.logsService = logsService;
+        this.groupsService = groupsService;
+        this.zipService = zipService;
+        RegularExpressionService = regularExpressionService;
     }
 
     protected ILogger<BaseConverter> Logger { get; }
 
+    protected IRegularExpressionService RegularExpressionService { get; }
+
     protected abstract Task ProcessGroupAsync(Group group);
+
+    public async Task ConvertAsync(string crawlerName)
+    {
+        this.Logger.LogInformation($"Converter: {crawlerName} start.");
+
+        try
+        {
+            var crawlerId = await this.crawlersService.GetCrawlerIdAsync(crawlerName);
+            var identifiers = await this.logsService.GetLogIdentifiersAsync(crawlerId);
+
+            //identifiers = new List<Guid>
+            //{
+            //    Guid.Parse("aa427cab-7bf3-4ab6-ac2e-ba361d8395df")
+            //};
+
+            await identifiers.ParallelForEachAsync(async identifier =>
+            {
+                try
+                {
+                    var group = await this.groupsService.GetGroupAsync(identifier);
+                    var zipModels = this.zipService.UnzipGroup(group.Content);
+                    foreach (var document in group.Documents)
+                    {
+                        var zipModel = zipModels.First(z => z.Name == document.Name);
+                        document.Content = zipModel.Content;
+                    }
+
+                    await this.ProcessGroupAsync(group);
+                }
+                catch (Exception ex)
+                {
+                    this.Logger.LogError(ex, $"Group was not process: {identifier};");
+                }
+            }, maxDegreeOfParallelism: 1);
+        }
+        catch (Exception ex)
+        {
+            this.Logger.LogError(ex, $"Failed to process documents from converter: {crawlerName};");
+        }
+
+        this.Logger.LogInformation($"Converter: {crawlerName} end.");
+    }
+
+    protected HtmlDocument CreateHtmlDocument(Document document)
+    {
+        var encoding = Encoding.GetEncoding(document.Encoding);
+        var html = encoding.GetString(document.Content).Decode();
+        var htmlDocument = new HtmlDocument();
+        htmlDocument.LoadHtml(html);
+
+        return htmlDocument;
+    }
 }
