496d37881967d8ebd1e8447353d6e68d9a3baf32
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-07-09 14:07:17 +0300

    Create view model.

---
+++ src/Data/SportData.Data.ViewModels/Home/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SportData.Data.ViewModels.Home;
+
+public class HomeViewModel
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+}
