3c51f3083a19912fbf2b0e9cf0d888caf4827827
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-07-09 14:08:19 +0300

    A service created.

---
+++ src/Services/SportData.Services/RegularExpressionService.cs
@@ -0,0 +1,57 @@
+﻿namespace SportData.Services;
+
+using System.Text.RegularExpressions;
+
+using SportData.Common.Extensions;
+using SportData.Services.Interfaces;
+
+public class RegularExpressionService : IRegularExpressionService
+{
+    public string CutHtml(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return null;
+        }
+
+        return Regex.Replace(input, "<.*?>", string.Empty);
+    }
+
+    public bool IsMatch(string text, string pattern)
+    {
+        return Regex.IsMatch(text, pattern);
+    }
+
+    public Match Match(string text, string pattern)
+    {
+        var match = Regex.Match(text, pattern, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+        if (match.Success)
+        {
+            return match;
+        }
+
+        return null;
+    }
+
+    public MatchCollection Matches(string text, string pattern)
+    {
+        return Regex.Matches(text, pattern, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+    }
+
+    public string MatchFirstGroup(string text, string pattern)
+    {
+        var match = Regex.Match(text, pattern, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+        if (match.Success)
+        {
+            var result = match.Groups[1].Value.Trim().Decode();
+            return result;
+        }
+
+        return null;
+    }
+
+    public string Replace(string text, string pattern, string replacement)
+    {
+        return Regex.Replace(text, pattern, replacement);
+    }
+}
