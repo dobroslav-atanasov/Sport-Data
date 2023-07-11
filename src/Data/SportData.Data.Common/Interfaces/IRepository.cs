4ae8b11f3388ae01e709689c14e2080883c38ccd
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-07-11 13:32:00 +0300

    IRepository interface implemented.

---
+++ src/Data/SportData.Data.Common/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace SportData.Data.Common.Interfaces;
+
+using System.Linq.Expressions;
+
+public interface IRepository<TEntity> : IDisposable
+    where TEntity : class
+{
+    Task AddAsync(TEntity entity);
+
+    Task<TEntity> GetAsync(params object[] id);
+
+    Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> expression);
+
+    Task<int> SaveChangesAsync();
+}
