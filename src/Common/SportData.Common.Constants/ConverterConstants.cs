09812508c6fa0eeb8ce3ae5de6613e5c11895ad4
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-07-12 10:33:33 +0300

    File added.

---
+++ src/Common/SportData.Common.Constants/ConverterConstants.cs
@@ -0,0 +1,6 @@
+﻿namespace SportData.Common.Constants;
+
+public static class ConverterConstants
+{
+    public const string COUNTRY_CONVERTER = "SportData.Crawlers.Countries.CountryDataCrawler";
+}
