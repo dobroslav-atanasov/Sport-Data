ed2db970ee651506f79a5a67e300394e239212be
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-07-12 10:35:04 +0300

    Functions All, AllAsNoTracking and Update added.

--- src/Data/SportData.Data.Common/Interfaces/IRepository.cs
+++ src/Data/SportData.Data.Common/Interfaces/IRepository.cs
@@ -7,9 +7,15 @@ public interface IRepository<TEntity> : IDisposable
 {
     Task AddAsync(TEntity entity);
 
+    IQueryable<TEntity> All();
+
+    IQueryable<TEntity> AllAsNoTracking();
+
     Task<TEntity> GetAsync(params object[] id);
 
     Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> expression);
 
     Task<int> SaveChangesAsync();
+
+    void Update(TEntity entity);
 }
