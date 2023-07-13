c43d5e30aa135e53c7bde35bc4b6e7c42b901235
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-07-13 11:28:39 +0300

    Entity "Game" added.

---
+++ src/Data/SportData.Data.Entities/OlympicGames/Game.cs
@@ -0,0 +1,27 @@
+﻿namespace SportData.Data.Entities.OlympicGames;
+
+using SportData.Data.Common.Interfaces;
+using SportData.Data.Common.Models;
+
+public class Game : BaseEntity<int>, IDeletableEntity, IEquatable<Game>
+{
+
+    public bool IsDeleted { get; set; }
+
+    public DateTime? DeletedOn { get; set; }
+
+    public bool Equals(Game other)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool Equals(object obj)
+    {
+        return Equals(obj as Game);
+    }
+
+    public override int GetHashCode()
+    {
+        throw new NotImplementedException();
+    }
+}
