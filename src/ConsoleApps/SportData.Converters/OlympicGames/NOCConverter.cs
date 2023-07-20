9d25de2c0cdf8bb7a2b15e6177a96131e0a0c8af
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-07-20 15:55:03 +0300

    Converter create

---
+++ src/ConsoleApps/SportData.Converters/OlympicGames/NOCConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace SportData.Converters.OlympicGames;
+
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using SportData.Data.Entities.Crawlers;
+using SportData.Services.Data.CrawlerStorage.Interfaces;
+using SportData.Services.Interfaces;
+
+public class NOCConverter : BaseConverter
+{
+    public NOCConverter(ILogger<BaseConverter> logger, ICrawlersService crawlersService, ILogsService logsService, IGroupsService groupsService, IZipService zipService,
+        IRegExpService regExpService)
+        : base(logger, crawlersService, logsService, groupsService, zipService, regExpService)
+    {
+    }
+
+    protected override async Task ProcessGroupAsync(Group group)
+    {
+        throw new NotImplementedException();
+    }
+}
