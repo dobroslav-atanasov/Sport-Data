a2e5ede0386b6d680d858be0db6e394e53250b91
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-07-25 10:14:38 +0300

    Namespaces changed.

---
+++ src/Services/SportData.Services.Data/CrawlerStorageDb/CrawlersService.cs
@@ -0,0 +1,38 @@
+﻿namespace SportData.Services.Data.CrawlerStorageDb;
+
+using System.Threading.Tasks;
+
+using SportData.Data.Common.Interfaces;
+using SportData.Data.Entities.Crawlers;
+using SportData.Data.Repositories;
+using SportData.Services.Data.CrawlerStorageDb.Interfaces;
+
+public class CrawlersService : ICrawlersService
+{
+    private readonly IRepository<Crawler> repository;
+
+    public CrawlersService(CrawlerStorageRepository<Crawler> repository)
+    {
+        this.repository = repository;
+    }
+
+    public async Task AddCrawler(string crawlerName)
+    {
+        var crawler = new Crawler { Name = crawlerName };
+        await this.repository.AddAsync(crawler);
+        await this.repository.SaveChangesAsync();
+    }
+
+    public async Task<int> GetCrawlerIdAsync(string crawlerName)
+    {
+        var crawler = await this.repository.GetAsync(x => x.Name == crawlerName);
+
+        if (crawler == null)
+        {
+            await this.AddCrawler(crawlerName);
+            return await this.GetCrawlerIdAsync(crawlerName);
+        }
+
+        return crawler.Id;
+    }
+}
