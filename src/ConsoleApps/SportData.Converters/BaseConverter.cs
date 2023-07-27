499e1785fda132b5e453281ce71d2b5f10046942
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-07-27 03:17:21 +0300

    Using changed and add new property (INormalizeService).

--- src/ConsoleApps/SportData.Converters/BaseConverter.cs
+++ src/ConsoleApps/SportData.Converters/BaseConverter.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Logging;
 
 using SportData.Common.Extensions;
 using SportData.Data.Entities.Crawlers;
-using SportData.Services.Data.CrawlerStorage.Interfaces;
+using SportData.Services.Data.CrawlerStorageDb.Interfaces;
 using SportData.Services.Interfaces;
 
 public abstract class BaseConverter
@@ -21,7 +21,7 @@ public abstract class BaseConverter
     private readonly IZipService zipService;
 
     public BaseConverter(ILogger<BaseConverter> logger, ICrawlersService crawlersService, ILogsService logsService, IGroupsService groupsService, IZipService zipService,
-        IRegExpService regExpService)
+        IRegExpService regExpService, INormalizeService normalizeService)
     {
         this.Logger = logger;
         this.crawlersService = crawlersService;
@@ -29,11 +29,14 @@ public abstract class BaseConverter
         this.groupsService = groupsService;
         this.zipService = zipService;
         this.RegExpService = regExpService;
+        this.NormalizeService = normalizeService;
     }
 
     protected ILogger<BaseConverter> Logger { get; }
 
     protected IRegExpService RegExpService { get; }
+
+    protected INormalizeService NormalizeService { get; }
 
     protected abstract Task ProcessGroupAsync(Group group);
 
