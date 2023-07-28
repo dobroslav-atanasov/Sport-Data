f3d7851e0dde72c831c3018d1219bbdc5841049d
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-07-28 10:51:48 +0300

    Constant added.

--- src/Common/SportData.Common.Constants/ConverterConstants.cs
+++ src/Common/SportData.Common.Constants/ConverterConstants.cs
@@ -7,4 +7,5 @@ public static class ConverterConstants
     public const string OLYMPEDIA_GAME_CONVERTER = "SportData.Crawlers.Olympedia.GameCrawler";
     public const string OLYMPEDIA_SPORT_DISCIPLINE_CONVERTER = "SportData.Crawlers.Olympedia.SportDisciplineCrawler";
     public const string OLYMPEDIA_VENUE_CONVERTER = "SportData.Crawlers.Olympedia.VenueCrawler";
+    public const string OLYMPEDIA_RESULT_CONVERTER = "SportData.Crawlers.Olympedia.ResultCrawler";
 }
