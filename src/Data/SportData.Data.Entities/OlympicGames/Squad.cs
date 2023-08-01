66006dea13ca86c3f4a33a700844d6c76e7f333a
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-08-01 10:14:06 +0300

    Entity added.

---
+++ src/Data/SportData.Data.Entities/OlympicGames/Squad.cs
@@ -0,0 +1,23 @@
+﻿namespace SportData.Data.Entities.OlympicGames;
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+using SportData.Data.Common.Interfaces;
+
+[Table("Squads", Schema = "og")]
+public class Squad : ICheckableEntity, IDeletableEntity
+{
+    public Guid ParticipantId { get; set; }
+    public virtual Participant Participant { get; set; }
+
+    public Guid TeamId { get; set; }
+    public virtual Team Team { get; set; }
+
+    public DateTime CreatedOn { get; set; }
+
+    public DateTime? ModifiedOn { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public DateTime? DeletedOn { get; set; }
+}
