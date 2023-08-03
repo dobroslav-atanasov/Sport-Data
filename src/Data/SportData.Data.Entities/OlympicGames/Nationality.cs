9a74e3b315cc5b1a130331c037565964156d9230
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-08-03 08:25:00 +0300

    Entity updated.

--- src/Data/SportData.Data.Entities/OlympicGames/Nationality.cs
+++ src/Data/SportData.Data.Entities/OlympicGames/Nationality.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using SportData.Data.Common.Interfaces;
 
 [Table("Nationalities", Schema = "og")]
-public class Nationality : ICheckableEntity, IDeletableEntity, IEquatable<Nationality>
+public class Nationality : ICheckableEntity, IDeletableEntity
 {
     public Guid AthleteId { get; set; }
     public virtual Athlete Athlete { get; set; }
@@ -20,25 +20,4 @@ public class Nationality : ICheckableEntity, IDeletableEntity, IEquatable<Nation
     public bool IsDeleted { get; set; }
 
     public DateTime? DeletedOn { get; set; }
-
-    public bool Equals(Nationality other)
-    {
-        if (other == null)
-        {
-            return false;
-        }
-
-        return this.AthleteId == other.AthleteId
-            && this.NOCId == other.NOCId;
-    }
-
-    public override bool Equals(object obj)
-    {
-        return Equals(obj as Nationality);
-    }
-
-    public override int GetHashCode()
-    {
-        return $"{this.AthleteId}-{this.NOCId}".GetHashCode();
-    }
 }
