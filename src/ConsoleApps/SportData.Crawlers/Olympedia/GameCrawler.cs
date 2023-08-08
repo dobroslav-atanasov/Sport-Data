9b5a4bcc0d52fee4369226ba2b6fd500f669b207
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-08-08 08:52:10 +0300

    A crawler has been updated.

--- src/ConsoleApps/SportData.Crawlers/Olympedia/GameCrawler.cs
+++ src/ConsoleApps/SportData.Crawlers/Olympedia/GameCrawler.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 using SportData.Common.Constants;
-using SportData.Services.Data.CrawlerStorage.Interfaces;
+using SportData.Services.Data.CrawlerStorageDb.Interfaces;
 using SportData.Services.Interfaces;
 
 public class GameCrawler : BaseOlympediaCrawler
