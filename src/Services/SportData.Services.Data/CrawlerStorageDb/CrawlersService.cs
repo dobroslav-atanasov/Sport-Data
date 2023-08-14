e8b231d61a9ab2bd193675e6262fc8282e201333
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-08-14 08:30:39 +0300

    The service has been updated.

--- src/Services/SportData.Services.Data/CrawlerStorageDb/CrawlersService.cs
+++ src/Services/SportData.Services.Data/CrawlerStorageDb/CrawlersService.cs
@@ -2,30 +2,38 @@
 
 using System.Threading.Tasks;
 
-using SportData.Data.Common.Interfaces;
 using SportData.Data.Entities.Crawlers;
+using SportData.Data.Factories.Interfaces;
 using SportData.Data.Repositories;
 using SportData.Services.Data.CrawlerStorageDb.Interfaces;
 
 public class CrawlersService : ICrawlersService
 {
-    private readonly IRepository<Crawler> repository;
+    //private readonly IRepository<Crawler> repository;
+    private readonly IDbContextFactory dbContextFactory;
 
-    public CrawlersService(CrawlerStorageRepository<Crawler> repository)
+    public CrawlersService(CrawlerStorageRepository<Crawler> repository, IDbContextFactory dbContextFactory)
     {
-        this.repository = repository;
+        //this.repository = repository;
+        this.dbContextFactory = dbContextFactory;
     }
 
     public async Task AddCrawler(string crawlerName)
     {
+        using var context = this.dbContextFactory.CreateCrawlerStorageDbContext();
         var crawler = new Crawler { Name = crawlerName };
-        await this.repository.AddAsync(crawler);
-        await this.repository.SaveChangesAsync();
+        //await this.repository.AddAsync(crawler);
+        //await this.repository.SaveChangesAsync();
+
+        await context.AddAsync(crawler);
+        await context.SaveChangesAsync();
     }
 
     public async Task<int> GetCrawlerIdAsync(string crawlerName)
     {
-        var crawler = await this.repository.GetAsync(x => x.Name == crawlerName);
+        using var context = this.dbContextFactory.CreateCrawlerStorageDbContext();
+        var crawler = context.Crawlers.FirstOrDefault(x => x.Name == crawlerName);
+        //var crawler = await this.repository.GetAsync(x => x.Name == crawlerName);
 
         if (crawler == null)
         {
