2b65d1b41af1562fea92ce19d70613bc62a35741
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-08-14 08:32:57 +0300

    A service has been added.

---
+++ src/Services/SportData.Services.Data/CrawlerStorageDb/OperationsService.cs
@@ -0,0 +1,39 @@
+﻿namespace SportData.Services.Data.CrawlerStorageDb;
+
+using SportData.Data.Entities.Crawlers;
+using SportData.Data.Factories.Interfaces;
+using SportData.Data.Repositories;
+using SportData.Services.Data.CrawlerStorageDb.Interfaces;
+
+public class OperationsService : IOperationsService
+{
+    private readonly IDbContextFactory dbContextFactory;
+
+    //private readonly CrawlerStorageRepository<Operation> repository;
+
+    public OperationsService(CrawlerStorageRepository<Operation> repository, IDbContextFactory dbContextFactory)
+    {
+        this.dbContextFactory = dbContextFactory;
+        //this.repository = repository;
+    }
+
+    public async Task AddOperationAsync(string operationName)
+    {
+        using var context = this.dbContextFactory.CreateCrawlerStorageDbContext();
+
+        var operation = new Operation { Name = operationName };
+        //await this.repository.AddAsync(operation);
+        //await this.repository.SaveChangesAsync();
+
+        await context.AddAsync(operation);
+        await context.SaveChangesAsync();
+    }
+
+    public bool IsOperationTableFull()
+    {
+        using var context = this.dbContextFactory.CreateCrawlerStorageDbContext();
+        return context.Operations.Any();
+
+        //return this.repository.All().Any();
+    }
+}
