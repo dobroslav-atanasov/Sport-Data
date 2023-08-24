be18a15dfd86909366eb674411ca7ce8d46613c1
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-08-24 08:51:23 +0300

    Enumeration created.

---
+++ src/Data/SportData.Data.Entities/Enumerations/RoundType.cs
@@ -0,0 +1,43 @@
+﻿namespace SportData.Data.Entities.Enumerations;
+
+public enum RoundType
+{
+    None = 0,
+    RoundRobin = 1,
+    FinalRound = 2,
+    Classification = 3,
+    Semifinals = 4,
+    Quaterfinals = 5,
+    Group = 6,
+    RoundOne = 7,
+    RoundOneRepechage = 8,
+    RoundTwo = 9,
+    RoundTwoRepechage = 10,
+    RoundThree = 11,
+    RankingRound = 12,
+    Final = 13,
+    Qualification = 14,
+
+    //GoldMedal = 1,
+    //BronzeMedal = 2,
+    //Semifinals = 3,
+    //Quaterfinals = 4,
+    //RoundRobin = 5,
+    //FinalRound = 6,
+    //QualificationRoundOne = 7,
+    //QualificationRoundTwo = 8,
+    //Classification = 9,
+    //Final = 10,
+    //RoundOne = 11,
+    //RoundTwo = 12,
+    //RoundThree = 13,
+    //RankingRound = 14,
+    //Total = 15,
+    //Qualification = 16,
+    //ClassificationRoundFiveEight = 17,
+    //PlaceMatchFive = 18,
+    //PlaceMatchSeven = 19,
+    //Repechage = 20,
+
+
+}
