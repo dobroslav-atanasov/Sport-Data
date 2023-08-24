256a6a6815d2fe46e0c81b69c447838df765f033
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-08-24 08:52:04 +0300

    An entity added.

---
+++ src/Data/SportData.Data.Entities/OlympicGames/Result.cs
@@ -0,0 +1,30 @@
+﻿namespace SportData.Data.Entities.OlympicGames;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using SportData.Data.Common.Interfaces;
+using SportData.Data.Common.Models;
+
+[Table("Results", Schema = "og")]
+public class Result : BaseDeletableEntity<Guid>, IUpdatable<Result>
+{
+    public int EventId { get; set; }
+    public virtual Event Event { get; set; }
+
+    [Required]
+    public string Json { get; set; }
+
+    public bool IsUpdated(Result other)
+    {
+        var isUpdated = false;
+
+        if (this.Json != other.Json)
+        {
+            this.Json = other.Json;
+            isUpdated = true;
+        }
+
+        return isUpdated;
+    }
+}
