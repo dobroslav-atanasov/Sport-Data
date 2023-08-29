bee8c5db652baa676697b1021566b15eca58a223
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-08-29 08:47:44 +0300

    Create cache model.

---
+++ src/Data/SportData.Data.Models/Cache/EventCacheModel.cs
@@ -0,0 +1,21 @@
+﻿namespace SportData.Data.Models.Cache;
+
+using SportData.Data.Entities.OlympicGames;
+using SportData.Services.Mapper.Interfaces;
+
+public class EventCacheModel : IMapFrom<Event>
+{
+    public int Id { get; set; }
+
+    public string OriginalName { get; set; }
+
+    public string Name { get; set; }
+
+    public string NormalizedName { get; set; }
+
+    public bool IsTeamEvent { get; set; }
+
+    public int DisciplineId { get; set; }
+
+    public int GameId { get; set; }
+}
