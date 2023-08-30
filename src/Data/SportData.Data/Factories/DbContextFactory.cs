7dd9e7f7d5e4a40fa60c04be753c61bed8d05367
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-08-30 08:28:21 +0300

    Db context factory has been created.

---
+++ src/Data/SportData.Data/Factories/DbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace SportData.Data.Factories;
+
+using Microsoft.EntityFrameworkCore;
+
+using SportData.Data.Contexts;
+using SportData.Data.Factories.Interfaces;
+
+public class DbContextFactory : IDbContextFactory
+{
+    private readonly DbContextOptions<CrawlerStorageDbContext> crawlerStorageDbOptions;
+    private readonly DbContextOptions<SportDataDbContext> sportStatsDbOptions;
+
+    public DbContextFactory(DbContextOptions<CrawlerStorageDbContext> crawlerStorageDbOptions, DbContextOptions<SportDataDbContext> sportStatsDbOptions)
+    {
+        this.crawlerStorageDbOptions = crawlerStorageDbOptions;
+        this.sportStatsDbOptions = sportStatsDbOptions;
+    }
+
+    public CrawlerStorageDbContext CreateCrawlerStorageDbContext()
+    {
+        return new CrawlerStorageDbContext(crawlerStorageDbOptions);
+    }
+
+    public SportDataDbContext CreateSportDataDbContext()
+    {
+        return new SportDataDbContext(sportStatsDbOptions);
+    }
+}
