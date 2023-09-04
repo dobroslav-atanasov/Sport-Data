f4e5d9bc3b8e9534533368166138c3a3f7d5a048
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-09-04 07:41:17 +0300

    Create a service.

---
+++ src/Services/SportData.Services.Data/SportDataDb/Interfaces/ICitiesService.cs
@@ -0,0 +1,13 @@
+﻿namespace SportData.Services.Data.SportDataDb.Interfaces;
+
+using SportData.Data.Entities.OlympicGames;
+using SportData.Data.Models.Cache;
+
+public interface ICitiesService
+{
+    Task<City> AddOrUpdateAsync(City city);
+
+    Task<City> GetAsync(string name);
+
+    ICollection<CityCacheModel> GetCityCacheModels();
+}
