c4b13cff22503adc7a94151a24032d97a04b3a93
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-09-04 07:40:50 +0300

    Create a service.

---
+++ src/Services/SportData.Services.Data/SportDataDb/Interfaces/IAthletesService.cs
@@ -0,0 +1,10 @@
+﻿namespace SportData.Services.Data.SportDataDb.Interfaces;
+
+using SportData.Data.Entities.OlympicGames;
+
+public interface IAthletesService
+{
+    Task<Athlete> AddOrUpdateAsync(Athlete athlete);
+
+    Task<Athlete> GetAsync(int number);
+}
