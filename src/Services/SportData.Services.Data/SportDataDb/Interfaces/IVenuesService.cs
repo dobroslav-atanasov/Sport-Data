335052433503dc8c88afab80cf2613508ba7f9fc
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-09-08 07:27:20 +0300

    Introduce the service.

---
+++ src/Services/SportData.Services.Data/SportDataDb/Interfaces/IVenuesService.cs
@@ -0,0 +1,11 @@
+﻿namespace SportData.Services.Data.SportDataDb.Interfaces;
+
+using SportData.Data.Entities.OlympicGames;
+using SportData.Data.Models.Cache;
+
+public interface IVenuesService
+{
+    Task<Venue> AddOrUpdateAsync(Venue venue);
+
+    ICollection<VenueCacheModel> GetVenueCacheModels();
+}
---
+++ src/Services/SportData.Services.Data/SportDataDb/VenuesService.cs
@@ -0,0 +1,53 @@
+﻿namespace SportData.Services.Data.SportDataDb;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using SportData.Data.Entities.OlympicGames;
+using SportData.Data.Models.Cache;
+using SportData.Data.Repositories;
+using SportData.Services.Data.SportDataDb.Interfaces;
+using SportData.Services.Mapper.Extensions;
+
+public class VenuesService : IVenuesService
+{
+    private readonly SportDataRepository<Venue> repository;
+
+    public VenuesService(SportDataRepository<Venue> repository)
+    {
+        this.repository = repository;
+    }
+
+    public async Task<Venue> AddOrUpdateAsync(Venue venue)
+    {
+        var dbVenue = await repository.GetAsync(x => x.Number == venue.Number);
+        if (dbVenue == null)
+        {
+            await repository.AddAsync(venue);
+            await repository.SaveChangesAsync();
+        }
+        else
+        {
+            var isUpdated = dbVenue.IsUpdated(venue);
+            if (isUpdated)
+            {
+                repository.Update(dbVenue);
+                await repository.SaveChangesAsync();
+            }
+
+            venue = dbVenue;
+        }
+
+        return venue;
+    }
+
+    public ICollection<VenueCacheModel> GetVenueCacheModels()
+    {
+        var models = this.repository
+            .AllAsNoTracking()
+            .To<VenueCacheModel>()
+            .ToList();
+
+        return models;
+    }
+}
