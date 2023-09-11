a1b10cc6bf73f0e74a267fdb05f6c65862d58e82
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-09-11 08:16:47 +0300

    Added service.

---
+++ src/Services/SportData.Services.Data/SportDataDb/Interfaces/ITeamsService.cs
@@ -0,0 +1,12 @@
+﻿namespace SportData.Services.Data.SportDataDb.Interfaces;
+
+using SportData.Data.Entities.OlympicGames;
+
+public interface ITeamsService
+{
+    Task<Team> AddOrUpdateAsync(Team team);
+
+    Task<Team> GetAsync(int nocId, int eventId);
+
+    Task<Team> GetAsync(string name, int nocId, int eventId);
+}
