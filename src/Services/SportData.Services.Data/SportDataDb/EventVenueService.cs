6caa36db4ad6386ef8df63584c965d25da44b426
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-09-12 07:45:22 +0300

    Created a service.

---
+++ src/Services/SportData.Services.Data/SportDataDb/EventVenueService.cs
@@ -0,0 +1,33 @@
+﻿namespace SportData.Services.Data.SportDataDb;
+
+using System.Threading.Tasks;
+
+using SportData.Data.Entities.OlympicGames;
+using SportData.Data.Repositories;
+using SportData.Services.Data.SportDataDb.Interfaces;
+
+public class EventVenueService : IEventVenueService
+{
+    private readonly SportDataRepository<EventVenue> repository;
+
+    public EventVenueService(SportDataRepository<EventVenue> repository)
+    {
+        this.repository = repository;
+    }
+
+    public async Task<EventVenue> AddOrUpdateAsync(EventVenue eventVenue)
+    {
+        var dbEventVenue = await repository.GetAsync(x => x.EventId == eventVenue.EventId && x.VenueId == eventVenue.VenueId);
+        if (dbEventVenue == null)
+        {
+            await repository.AddAsync(eventVenue);
+            await repository.SaveChangesAsync();
+        }
+        else
+        {
+            eventVenue = dbEventVenue;
+        }
+
+        return eventVenue;
+    }
+}
---
+++ src/Services/SportData.Services.Data/SportDataDb/Interfaces/IEventVenueService.cs
@@ -0,0 +1,8 @@
+﻿namespace SportData.Services.Data.SportDataDb.Interfaces;
+
+using SportData.Data.Entities.OlympicGames;
+
+public interface IEventVenueService
+{
+    Task<EventVenue> AddOrUpdateAsync(EventVenue eventVenue);
+}
