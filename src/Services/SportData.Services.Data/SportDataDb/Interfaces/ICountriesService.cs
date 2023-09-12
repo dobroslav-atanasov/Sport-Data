89bf41fa456342424552ff08913f1220b18eb1dd
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-09-12 07:44:08 +0300

    Created a service.

---
+++ src/Services/SportData.Services.Data/SportDataDb/Interfaces/ICountriesService.cs
@@ -0,0 +1,10 @@
+﻿namespace SportData.Services.Data.SportDataDb.Interfaces;
+
+using SportData.Data.Entities.Countries;
+
+public interface ICountriesService
+{
+    Task<Country> AddOrUpdateAsync(Country country);
+
+    Task<Country> GetAsync(string code);
+}
