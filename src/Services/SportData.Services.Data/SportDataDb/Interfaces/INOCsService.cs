bbf1b267c60e8a0bd8e00d19a775d89fba27a843
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-09-13 10:08:50 +0300

    Created a service.

---
+++ src/Services/SportData.Services.Data/SportDataDb/Interfaces/INOCsService.cs
@@ -0,0 +1,11 @@
+﻿namespace SportData.Services.Data.SportDataDb.Interfaces;
+
+using SportData.Data.Entities.OlympicGames;
+using SportData.Data.Models.Cache;
+
+public interface INOCsService
+{
+    Task<NOC> AddOrUpdateAsync(NOC noc);
+
+    ICollection<NOCCacheModel> GetNOCCacheModels();
+}
