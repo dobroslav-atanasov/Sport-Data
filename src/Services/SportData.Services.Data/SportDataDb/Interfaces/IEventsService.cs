374978411524a0224061b09b2f05b879ffd9d281
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-09-13 10:08:00 +0300

    Created a service.

---
+++ src/Services/SportData.Services.Data/SportDataDb/Interfaces/IEventsService.cs
@@ -0,0 +1,11 @@
+﻿namespace SportData.Services.Data.SportDataDb.Interfaces;
+
+using SportData.Data.Entities.OlympicGames;
+using SportData.Data.Models.Cache;
+
+public interface IEventsService
+{
+    Task<Event> AddOrUpdateAsync(Event @event);
+
+    ICollection<EventCacheModel> GetEventCacheModels();
+}
