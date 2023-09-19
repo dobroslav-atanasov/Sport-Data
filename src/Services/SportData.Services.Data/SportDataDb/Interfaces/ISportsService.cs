13034418075cac305b7cd087450a97263e082bd9
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-09-19 22:48:09 +0300

    A service has been created.

---
+++ src/Services/SportData.Services.Data/SportDataDb/Interfaces/ISportsService.cs
@@ -0,0 +1,8 @@
+﻿namespace SportData.Services.Data.SportDataDb.Interfaces;
+
+using SportData.Data.Entities.OlympicGames;
+
+public interface ISportsService
+{
+    Task<Sport> AddOrUpdateAsync(Sport sport);
+}
