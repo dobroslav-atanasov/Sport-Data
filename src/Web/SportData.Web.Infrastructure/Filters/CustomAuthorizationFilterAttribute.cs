5e79318c3235b013fca5ad74277d3d8969c048fe
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-09-19 22:49:15 +0300

    Custom filters has been created.

---
+++ src/Web/SportData.Web.Infrastructure/Filters/CustomAuthorizationFilterAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace SportData.Web.Infrastructure.Filters;
+
+using Microsoft.AspNetCore.Mvc.Filters;
+
+public class CustomAuthorizationFilterAttribute : IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        ;
+    }
+}
---
+++ src/Web/SportData.Web.Infrastructure/Filters/CustomExceptionFilterAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace SportData.Web.Infrastructure.Filters;
+
+using Microsoft.AspNetCore.Mvc.Filters;
+
+public class CustomExceptionFilterAttribute : ExceptionFilterAttribute
+{
+    public override void OnException(ExceptionContext context)
+    {
+        base.OnException(context);
+    }
+}
