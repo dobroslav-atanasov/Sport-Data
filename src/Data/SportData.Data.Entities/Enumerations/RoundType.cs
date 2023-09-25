934af0ba3d4624f1590f4ad5a02b59618810327c
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-09-25 08:53:27 +0300

    Removed comments.

--- src/Data/SportData.Data.Entities/Enumerations/RoundType.cs
+++ src/Data/SportData.Data.Entities/Enumerations/RoundType.cs
@@ -17,28 +17,7 @@ public enum RoundType
     RankingRound = 12,
     Final = 13,
     Qualification = 14,
-    Figures = 15
-
-    //GoldMedal = 1,
-    //BronzeMedal = 2,
-    //Semifinals = 3,
-    //Quaterfinals = 4,
-    //RoundRobin = 5,
-    //FinalRound = 6,
-    //QualificationRoundOne = 7,
-    //QualificationRoundTwo = 8,
-    //Classification = 9,
-    //Final = 10,
-    //RoundOne = 11,
-    //RoundTwo = 12,
-    //RoundThree = 13,
-    //RankingRound = 14,
-    //Total = 15,
-    //Qualification = 16,
-    //ClassificationRoundFiveEight = 17,
-    //PlaceMatchFive = 18,
-    //PlaceMatchSeven = 19,
-    //Repechage = 20,
-
-
+    Figures = 15,
+    Repechage = 16,
+    PreliminaryRound = 17,
 }
