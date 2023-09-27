5480b289ded67eb4c62735554f3cd124c680a5b1
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-09-27 09:25:38 +0300

    Added property.

--- src/Data/SportData.Data.Models/Cache/EventCacheModel.cs
+++ src/Data/SportData.Data.Models/Cache/EventCacheModel.cs
@@ -1,5 +1,6 @@
 ﻿namespace SportData.Data.Models.Cache;
 
+using SportData.Data.Entities.Enumerations;
 using SportData.Data.Entities.OlympicGames;
 using SportData.Services.Mapper.Interfaces;
 
@@ -12,6 +13,8 @@ public class EventCacheModel : IMapFrom<Event>
     public string Name { get; set; }
 
     public string NormalizedName { get; set; }
+
+    public GenderType Gender { get; set; }
 
     public bool IsTeamEvent { get; set; }
 
