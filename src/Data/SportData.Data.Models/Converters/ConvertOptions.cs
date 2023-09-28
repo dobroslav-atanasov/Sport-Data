8ef3b041eefe38282f0551f8fdb1e28621022367
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-09-28 20:16:45 +0300

    Added model.

---
+++ src/Data/SportData.Data.Models/Converters/ConvertOptions.cs
@@ -0,0 +1,23 @@
+﻿namespace SportData.Data.Models.Converters;
+
+using HtmlAgilityPack;
+
+using SportData.Data.Entities.Crawlers;
+using SportData.Data.Models.Cache;
+
+public class ConvertOptions
+{
+    public HtmlDocument HtmlDocument { get; set; }
+
+    public IOrderedEnumerable<Document> Documents { get; set; }
+
+    public GameCacheModel Game { get; set; }
+
+    public DisciplineCacheModel Discipline { get; set; }
+
+    public EventCacheModel Event { get; set; }
+
+    public TableModel StandingTable { get; set; }
+
+    public IList<TableModel> Tables { get; set; }
+}
