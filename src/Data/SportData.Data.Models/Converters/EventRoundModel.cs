832bcd14e713fbe1a48e1aa444185dc8a768d351
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-10-05 09:40:05 +0300

    Created model.

---
+++ src/Data/SportData.Data.Models/Converters/EventRoundModel.cs
@@ -0,0 +1,19 @@
+﻿namespace SportData.Data.Models.Converters;
+
+using SportData.Data.Models.Dates;
+
+public class EventRoundModel<TModel>
+{
+    public EventRoundModel()
+    {
+        this.Rounds = new List<TModel>();
+    }
+
+    public string Format { get; set; }
+
+    public DateTimeModel Dates { get; set; }
+
+    public string EventName { get; set; }
+
+    public List<TModel> Rounds { get; set; }
+}
