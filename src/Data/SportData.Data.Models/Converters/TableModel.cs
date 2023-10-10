074ef277a5b0cb4ebd95af7983594a787b1ecbf3
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-10-10 07:54:16 +0300

    Added model.

---
+++ src/Data/SportData.Data.Models/Converters/TableModel.cs
@@ -0,0 +1,35 @@
+﻿namespace SportData.Data.Models.Converters;
+
+using HtmlAgilityPack;
+
+using SportData.Data.Entities.Enumerations;
+
+public class TableModel
+{
+    public string Html { get; set; }
+
+    public int EventId { get; set; }
+
+    public HtmlDocument HtmlDocument
+    {
+        get
+        {
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(this.Html);
+
+            return htmlDocument;
+        }
+    }
+
+    public string Title { get; set; }
+
+    public RoundType Round { get; set; }
+
+    public string RoundInfo { get; set; }
+
+    public GroupType GroupType { get; set; }
+
+    public DateTime? FromDate { get; set; }
+
+    public DateTime? ToDate { get; set; }
+}
