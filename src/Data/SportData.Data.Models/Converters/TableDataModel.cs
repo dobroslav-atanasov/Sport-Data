11eccb9ba90924ed7ecd3edea1131471ac7eef95
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-10-10 07:54:05 +0300

    Added model.

---
+++ src/Data/SportData.Data.Models/Converters/TableDataModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SportData.Data.Models.Converters;
+
+using HtmlAgilityPack;
+
+public class TableDataModel
+{
+    public HtmlNodeCollection Rows { get; set; }
+
+    public List<string> Headers { get; set; }
+
+    public Dictionary<string, int> Indexes { get; set; }
+}
