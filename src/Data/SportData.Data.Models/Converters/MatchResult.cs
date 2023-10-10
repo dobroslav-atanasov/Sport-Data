71f781c7cdf9800a0c6a4926648310bf9b94ea9d
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-10-10 07:53:35 +0300

    Added model.

---
+++ src/Data/SportData.Data.Models/Converters/MatchResult.cs
@@ -0,0 +1,24 @@
+﻿namespace SportData.Data.Models.Converters;
+
+using SportData.Data.Entities.Enumerations;
+
+public class MatchResult
+{
+    public int Points1 { get; set; }
+
+    public TimeSpan? Time1 { get; set; }
+
+    public ResultType Result1 { get; set; }
+
+    public int Points2 { get; set; }
+
+    public TimeSpan? Time2 { get; set; }
+
+    public ResultType Result2 { get; set; }
+
+    //public DecisionType Decision { get; set; }
+
+    public List<int?> Games1 { get; set; }
+
+    public List<int?> Games2 { get; set; }
+}
