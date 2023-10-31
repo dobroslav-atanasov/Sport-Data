91128651430410ada8380e61bf2526298357129f
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-10-31 10:03:55 +0200

    Base athlete model was added.

---
+++ src/Data/SportData.Data.Models/OlympicGames/Base/BaseAthlete.cs
@@ -0,0 +1,12 @@
+﻿namespace SportData.Data.Models.OlympicGames.Base;
+
+public class BaseAthlete
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+
+    public int Code { get; set; }
+
+    public string NOC { get; set; }
+}
