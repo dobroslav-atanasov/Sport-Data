724e0f9cce80995d3cb1145f64024ff7e528c953
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-11-03 07:32:09 +0200

    Create model.

---
+++ src/Data/SportData.Data.Models/Converters/ResultModel.cs
@@ -0,0 +1,16 @@
+﻿namespace SportData.Data.Models.Converters;
+
+public class ResultModel
+{
+    public int? Int { get; set; }
+
+    public double? Double { get; set; }
+
+    public decimal? Decimal { get; set; }
+
+    public TimeSpan? Time { get; set; }
+
+    public DateTime? Date { get; set; }
+
+    public string String { get; set; }
+}
