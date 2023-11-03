622085532209f98ee75425cb4120ff1275d8df71
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-11-03 07:33:10 +0200

    Models create.

---
+++ src/Data/SportData.Data.Models/OlympicGames/Aquatics/Diving/DIVPair.cs
@@ -0,0 +1,21 @@
+﻿namespace SportData.Data.Models.OlympicGames.Aquatics.Diving;
+
+using SportData.Data.Entities.Enumerations;
+using SportData.Data.Models.OlympicGames.Base;
+
+public class DIVPair : BaseTeam
+{
+    public DIVPair()
+    {
+        this.Divers = new List<DIVDiver>();
+        this.Dives = new List<DIVDive>();
+    }
+
+    public decimal? Points { get; set; }
+
+    public FinishStatus FinishStatus { get; set; }
+
+    public List<DIVDiver> Divers { get; set; }
+
+    public List<DIVDive> Dives { get; set; }
+}
---
+++ src/Data/SportData.Data.Models/OlympicGames/Aquatics/Diving/DIVRound.cs
@@ -0,0 +1,19 @@
+﻿namespace SportData.Data.Models.OlympicGames.Aquatics.Diving;
+
+using SportData.Data.Models.OlympicGames.Base;
+
+public class DIVRound : BaseRound
+{
+    public DIVRound()
+    {
+        this.Judges = new List<BaseJudge>();
+        this.Divers = new List<DIVDiver>();
+        this.Pairs = new List<DIVPair>();
+    }
+
+    public List<BaseJudge> Judges { get; set; }
+
+    public List<DIVDiver> Divers { get; set; }
+
+    public List<DIVPair> Pairs { get; set; }
+}
