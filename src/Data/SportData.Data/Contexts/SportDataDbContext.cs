71019b7f7bb7593a0a140ec5ca2fb369326f4330
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-11-06 09:59:56 +0200

    The db sets were moved to OlympicGamesDbContext.

--- src/Data/SportData.Data/Contexts/SportDataDbContext.cs
+++ src/Data/SportData.Data/Contexts/SportDataDbContext.cs
@@ -6,7 +6,6 @@ using Microsoft.EntityFrameworkCore;
 using SportData.Data.Common.Interfaces;
 using SportData.Data.Entities;
 using SportData.Data.Entities.Countries;
-using SportData.Data.Entities.OlympicGames;
 
 public class SportDataDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
 {
@@ -16,36 +15,6 @@ public class SportDataDbContext : IdentityDbContext<ApplicationUser, Application
     }
 
     public virtual DbSet<Country> Countries { get; set; }
-
-    public virtual DbSet<Athlete> Athletes { get; set; }
-
-    public virtual DbSet<City> Cities { get; set; }
-
-    public virtual DbSet<Discipline> Disciplines { get; set; }
-
-    public virtual DbSet<Event> Events { get; set; }
-
-    public virtual DbSet<EventVenue> EventVenues { get; set; }
-
-    public virtual DbSet<Game> Games { get; set; }
-
-    public virtual DbSet<Host> Hosts { get; set; }
-
-    public virtual DbSet<Nationality> Nationalities { get; set; }
-
-    public virtual DbSet<NOC> NOCs { get; set; }
-
-    public virtual DbSet<Participant> Participants { get; set; }
-
-    public virtual DbSet<Result> Results { get; set; }
-
-    public virtual DbSet<Sport> Sports { get; set; }
-
-    public virtual DbSet<Squad> Squads { get; set; }
-
-    public virtual DbSet<Team> Teams { get; set; }
-
-    public virtual DbSet<Venue> Venues { get; set; }
 
     public override int SaveChanges()
     {
@@ -87,70 +56,5 @@ public class SportDataDbContext : IdentityDbContext<ApplicationUser, Application
                 entity.ModifiedOn = DateTime.UtcNow;
             }
         }
-    }
-
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        base.OnModelCreating(builder);
-
-        builder.Entity<Host>()
-            .HasKey(h => new { h.CityId, h.GameId });
-
-        builder.Entity<Host>()
-            .HasOne(h => h.City)
-            .WithMany(c => c.Hosts)
-            .HasForeignKey(h => h.CityId)
-            .OnDelete(DeleteBehavior.ClientSetNull);
-
-        builder.Entity<Host>()
-            .HasOne(h => h.Game)
-            .WithMany(g => g.Hosts)
-            .HasForeignKey(h => h.GameId)
-            .OnDelete(DeleteBehavior.ClientSetNull);
-
-        builder.Entity<EventVenue>()
-             .HasKey(ev => new { ev.EventId, ev.VenueId });
-
-        builder.Entity<EventVenue>()
-            .HasOne(ev => ev.Event)
-            .WithMany(e => e.EventVenues)
-            .HasForeignKey(ev => ev.EventId)
-            .OnDelete(DeleteBehavior.ClientSetNull);
-
-        builder.Entity<EventVenue>()
-            .HasOne(ev => ev.Venue)
-            .WithMany(v => v.EventVenues)
-            .HasForeignKey(ev => ev.VenueId)
-            .OnDelete(DeleteBehavior.ClientSetNull);
-
-        builder.Entity<Nationality>()
-            .HasKey(n => new { n.AthleteId, n.NOCId });
-
-        builder.Entity<Nationality>()
-            .HasOne(n => n.Athlete)
-            .WithMany(a => a.Nationalities)
-            .HasForeignKey(n => n.AthleteId)
-            .OnDelete(DeleteBehavior.ClientSetNull);
-
-        builder.Entity<Nationality>()
-            .HasOne(n => n.NOC)
-            .WithMany(noc => noc.Nationalities)
-            .HasForeignKey(n => n.NOCId)
-            .OnDelete(DeleteBehavior.ClientSetNull);
-
-        builder.Entity<Squad>()
-            .HasKey(s => new { s.ParticipantId, s.TeamId });
-
-        builder.Entity<Squad>()
-            .HasOne(s => s.Participant)
-            .WithMany(p => p.Squads)
-            .HasForeignKey(s => s.ParticipantId)
-            .OnDelete(DeleteBehavior.ClientSetNull);
-
-        builder.Entity<Squad>()
-            .HasOne(s => s.Team)
-            .WithMany(t => t.Squads)
-            .HasForeignKey(s => s.TeamId)
-            .OnDelete(DeleteBehavior.ClientSetNull);
     }
 }
