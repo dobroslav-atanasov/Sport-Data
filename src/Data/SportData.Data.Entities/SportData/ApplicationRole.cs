41e73289f43da44ce9223a4349f67d443b2a71fe
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-11-06 10:46:44 +0200

    Entities added.

---
+++ src/Data/SportData.Data.Entities/SportData/ApplicationRole.cs
@@ -0,0 +1,16 @@
+﻿namespace SportData.Data.Entities.SportData;
+
+using global::SportData.Data.Common.Interfaces;
+
+using Microsoft.AspNetCore.Identity;
+
+public class ApplicationRole : IdentityRole, ICheckableEntity, IDeletableEntity
+{
+    public DateTime CreatedOn { get; set; }
+
+    public DateTime? ModifiedOn { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public DateTime? DeletedOn { get; set; }
+}
---
+++ src/Data/SportData.Data.Entities/SportData/ApplicationUser.cs
@@ -0,0 +1,26 @@
+﻿namespace SportData.Data.Entities.SportData;
+
+using global::SportData.Data.Common.Interfaces;
+
+using Microsoft.AspNetCore.Identity;
+
+public class ApplicationUser : IdentityUser, ICheckableEntity, IDeletableEntity
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Nickname { get; set; }
+
+    public DateTime? DateOfBirth { get; set; }
+
+    public string ImageUrl { get; set; }
+
+    public DateTime CreatedOn { get; set; }
+
+    public DateTime? ModifiedOn { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public DateTime? DeletedOn { get; set; }
+}
