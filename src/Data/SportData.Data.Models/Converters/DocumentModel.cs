eaa0ac8f5b376a00be73c435fdb02ad8eacca2ce
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-11-28 08:25:19 +0200

    DocumentModel created.

---
+++ src/Data/SportData.Data.Models/Converters/DocumentModel.cs
@@ -0,0 +1,18 @@
+﻿namespace SportData.Data.Models.Converters;
+
+using HtmlAgilityPack;
+
+public class DocumentModel
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+
+    public DateTime? From { get; set; }
+
+    public string Html { get; set; }
+
+    public HtmlDocument HtmlDocument { get; set; }
+
+    public List<RoundTableModel> Tables { get; set; } = new();
+}
