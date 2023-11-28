86b93ce88e4fc8f8cfe37db46e5499312dec067a
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-11-28 08:25:50 +0200

    SquadsService created.

---
+++ src/Services/SportData.Services.Data/OlympicGamesDb/Interfaces/ISquadsService.cs
@@ -0,0 +1,8 @@
+﻿namespace SportData.Services.Data.OlympicGamesDb.Interfaces;
+
+using SportData.Data.Entities.OlympicGames;
+
+public interface ISquadsService
+{
+    Task<Squad> AddOrUpdateAsync(Squad squad);
+}
