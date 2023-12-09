6ff2a7dcfbe53e8124c8660abcad5a4c10c61f27
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-12-09 12:15:09 +0200

    Model added.

---
+++ src/Data/SportData.Data.Models/Converters/RoundModel.cs
@@ -0,0 +1,16 @@
+﻿namespace SportData.Data.Models.Converters;
+
+using SportData.Data.Entities.Enumerations;
+
+public class RoundModel
+{
+    public string Name { get; set; }
+
+    public RoundStatus Status { get; set; }
+
+    public RoundStatus SubStatus { get; set; }
+
+    public int Group { get; set; }
+
+    public string Description { get; set; }
+}
