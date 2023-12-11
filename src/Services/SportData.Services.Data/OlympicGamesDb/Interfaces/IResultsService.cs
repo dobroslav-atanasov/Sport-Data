069cf44fca688bc31cc63edd620aedd26cfb5b2e
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2023-12-11 09:37:12 +0200

    Service created.

---
+++ src/Services/SportData.Services.Data/OlympicGamesDb/Interfaces/IResultsService.cs
@@ -0,0 +1,8 @@
+﻿namespace SportData.Services.Data.OlympicGamesDb.Interfaces;
+
+using SportData.Data.Entities.OlympicGames;
+
+public interface IResultsService
+{
+    Task<Result> AddOrUpdateAsync(Result result);
+}
