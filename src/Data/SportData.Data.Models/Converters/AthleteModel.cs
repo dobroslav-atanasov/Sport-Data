4bae9c6f3d3080d08fd9672e150f2550c6f74f31
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-01-02 14:50:21 +0200

    Property deleted.

--- src/Data/SportData.Data.Models/Converters/AthleteModel.cs
+++ src/Data/SportData.Data.Models/Converters/AthleteModel.cs
@@ -1,12 +1,7 @@
 ﻿namespace SportData.Data.Models.Converters;
-
-using SportData.Data.Entities.OlympicGames.Enumerations;
-
 public class AthleteModel
 {
     public int Code { get; set; }
-
-    public FinishStatus FinishStatus { get; set; }
 
     public string Name { get; set; }
 }
